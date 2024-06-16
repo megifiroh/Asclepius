ae72d3327c7492316be30ee8f862db66759c50a7
megifiroh <160246587+megifiroh@users.noreply.github.com>
2024-06-16 19:36:26 +0700

    Update loadModel.js

--- src/services/loadModel.js
+++ src/services/loadModel.js
@@ -2,18 +2,8 @@ require('dotenv').config();
 
 const tf = require('@tensorflow/tfjs-node');
 
-// disable this to use cloud storage
-const path = require('path');
-const modelDir = path.join(__dirname, 'model');
-const localModelPath = path.join(modelDir, 'model.json');
-
-
 async function loadModel() {
-  // disable to use local path
-  return tf.loadGraphModel(`file://${localModelPath}`);
-
-  // enable to use from cloudstorage
-  // return tf.loadGraphModel(process.env.MODEL_URL);
+  return tf.loadGraphModel(process.env.MODEL_URL); 
 }
 
 module.exports = { loadModel };
